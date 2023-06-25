e3c0b600858ebf6e122345642485273bb019d8e8
TiagoMontes <tiagosantos6303@gmail.com>
2023-06-25 20:08:38 +0000

    <fix>: fixing some routes and adding a new model (Wallet and WalletAsset)

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -1,17 +1,17 @@
-import { Body, Controller, Delete, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 
 @Controller('assets')
 export class AssetsController {
-  constructor(private readonly assetsService: AssetsService) {}
+  constructor(private readonly assetsService: AssetsService) { }
+  
+  @Get()
+  getAll() {
+    return this.assetsService.getAll()
+  }
   
   @Post()
   create(@Body() body: { id: string; symbol: string; price: number }) { // @Body pega os dados da requisição e atribui aos valores de body
     return this.assetsService.create(body);
-  }
-
-  @Delete()
-  delete(@Body() body: { id: string } ) {
-    return this.assetsService.delete(body.id);
   }
 }
--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -7,18 +7,13 @@ export class AssetsService {
         // Precisamos instanciar o PrismaService para podermos ter acesso às utilidades do ORM
     }
 
+    getAll() {
+        return this.prismaService.asset.findMany()
+    }
+
     create(data: { id: string; symbol: string; price: number }) {
         return this.prismaService.asset.create({
             data, // Ele já aceita os valores definidos acima, não é preciso passar novamente
         })
     }
-
-    delete( id: string ) {
-        return this.prismaService.asset.delete({
-            where: {
-                id: id
-            }
-        })
-    }
-
 }
